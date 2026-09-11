f4e908e9d0ae3ceefe8079c2448c923c9841ac73
kuttydharshini07-art <kuttydharshini07@gmail.com>
2026-09-11 10:34:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,7 @@ class TechifyBots(Client):
     def __init__(self):
         super().__init__(
             name="RenameBot",
-            api_id=Config.API_ID,
+           'api_id=(Config.API_ID),'
             api_hash=Config.API_HASH,
             bot_token=Config.BOT_TOKEN,
             workers=200,
